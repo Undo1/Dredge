77cf11eceddda24708baf66a9564560dba815e79
Undo1 <parker@erwaysoftware.com>
2016-01-02 20:19:53 -0700

    Fix ordering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all.order("created_at DESC").includes(:user)
+    @comments = Comment.all.order("comment_creation_date DESC").includes(:user)
   end
 
   # GET /comments/1
